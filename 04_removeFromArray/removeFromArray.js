a14ad9c66e64cfaa2a0693434e503e64a304f3ae
shahrierNafis <shahriernafisog@gmail.com>
2022-10-25 10:41:13 +0600

    complete exercise removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    let array = arguments[0];
+    for (const value of arguments) {
+        if (typeof (value) == typeof (array)) continue;
+        let indexOfValue = array.indexOf(value)
+        while (indexOfValue !== -1) {
+            array.splice(indexOfValue, 1);
+            indexOfValue = array.indexOf(value)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
