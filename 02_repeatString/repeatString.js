3df7a57929e04af3f18eee4ad4d315356b27b362
shahrierNafis <shahriernafisog@gmail.com>
2022-10-25 10:07:00 +0600

    complete exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
 
+    let newString = "";
+
+    while (number) {
+        newString = newString.concat(string);
+        number--;
+    }
+    return newString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
