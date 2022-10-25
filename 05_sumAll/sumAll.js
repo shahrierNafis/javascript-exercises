665087dabd19746e9c8a32cd286b162421754018
shahrierNafis <shahriernafisog@gmail.com>
2022-10-25 14:38:43 +0600

    complete exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (!(Number.isInteger(start) && Number.isInteger(end)
+        && start > -1 && end > -1)) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    start < end ? end++ : end--;
+    while (start !== end) {
+        sum += start;
+        start < end + 1 ? start++ : start--;
+    }
+    return sum;
 };
-
+sumAll(4, 1);
 // Do not edit below this line
 module.exports = sumAll;
